53f0242d1866ea15c5114b8920462936c19fcaa4
Victor Vaknin Otte Hansen <vicvo@icloud.com>
2025-03-19 16:29:06 +0100

    So, the last commit did not fix anything, but now i HOPE that I've fixed it!

--- Sources/MapItemPicker/UI/Components/SearchCell.swift
+++ Sources/MapItemPicker/UI/Components/SearchCell.swift
@@ -28,16 +28,30 @@ struct SearchCell: View {
     var body: some View {
         Button(action: action) {
             HStack {
-                Circle()
-                    .fill(color)
-                    .overlay {
-                        Image(systemName: systemImageName)
-                            .resizable()
-                            .scaledToFit()
-                            .padding(6)
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 30)
+                if #available(iOS 16.0, *) {
+                    Circle()
+                        .fill(color.gradient)
+                        .overlay {
+                            Image(systemName: systemImageName)
+                                .resizable()
+                                .scaledToFit()
+                                .padding(6)
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 30)
+                } else {
+                    // Fallback on earlier versions
+                    Circle()
+                        .fill(color)
+                        .overlay {
+                            Image(systemName: systemImageName)
+                                .resizable()
+                                .scaledToFit()
+                                .padding(6)
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 30)
+                }
                 VStack(alignment: .leading) {
                     Text(title)
                         .font(.body.bold())
